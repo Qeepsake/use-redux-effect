fd38f18376f8f9a972866df96037335fd05c75d3
Amogh J <amoghjahagirdargsf@gmail.com>
2020-09-05 17:21:25 +0530

    Fix action reducer

--- src/reducers/action-reducer.js
+++ src/reducers/action-reducer.js
@@ -23,38 +23,9 @@ const initialState = {
  */
 export const actionReducer = (state = initialState, action) => {
   return {
-    ...state,/**
-     * @author Luke Brandon Farrell
-     * @description Application reducer.
-     */
-
-    /**
-     * Initial data.
-     */
-    const initialState = {
-      type: null,
-      payload: null,
-      meta: null,
-      error: false,
-      count: 0
-    };
-
-    /**
-     * Calculates the application state.
-     *
-     * @param state
-     * @param action
-     * @return {*}
-     */
-    export const actionReducer = (state = initialState, action) => {
-      return {
-        ...state,
-        ...action,
-        count: state.count + 1,
-      };
-    };
-
+    ...state,
     ...action,
     count: state.count + 1,
   };
 };
+
